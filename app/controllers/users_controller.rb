a6cbc01acee4b98941668f0792f2dab0c9118bba
yunen7836 <yunen7836@yahoo.co.jp>
2020-01-17 10:20:26 +0900

    login_routes_solve

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:new]
   before_action :set_user, only: [:show, :profile_show, :update]
-  before_action :move_to_login, only: [:show, :logout, :profile_show]
 
   def show
     @user = User.all
@@ -27,9 +26,7 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
-  def move_to_login
-    redirect_to new_user_session_path unless user_signed_in?
-  end
+
 
   def update_params
     params.require(:user).permit(:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :birthday, :email, :self_introduction)
