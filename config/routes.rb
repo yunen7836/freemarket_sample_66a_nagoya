bdc3996fbf9dc4f038b15e65143823882ca8ba78
yunen7836 <yunen7836@yahoo.co.jp>
2020-01-24 16:22:44 +0900

    category_search_new_finish

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 
   root "items#index"
-  
+  get "category/new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
