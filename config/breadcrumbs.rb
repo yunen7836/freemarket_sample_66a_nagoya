a01fff6de4949d51445f633694b185245545b89b
yunen7836 <yunen7836@yahoo.co.jp>
2020-01-30 14:05:34 +0900

    bread_search_ok

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -26,3 +26,28 @@ crumb :card do
   link "支払方法", card_index_path(current_user.id)
   parent :mypage
 end
+
+#カテゴリー検索
+crumb :parent do
+  category = Category.find(params[:id])
+  parent_category = Category.find((/\//.match("#{category.ancestry}")).post_match)
+  parent_parent_category = Category.find(parent_category.ancestry)
+  link "#{parent_parent_category.name}", "#"
+end
+
+crumb :child do
+  category = Category.find(params[:id])
+  parent_category = Category.find((/\//.match("#{category.ancestry}")).post_match)
+  link "#{parent_category.name}", "#"
+  parent :parent
+end
+
+crumb :grand_child do |category|
+  link "#{category.name}", "#"
+  parent :child
+end
+
+#テキスト検索
+crumb :search do |search_text|
+  link "検索ワード: #{search_text}"
+end
